613ead589554181d15ea00984f3bfd4f9ce0d8ff
laineysu <63746471+laineysu@users.noreply.github.com>
2021-12-12 21:02:30 +0000

    12Dec commit

--- script/script.js
+++ script/script.js
@@ -1,3 +1,6 @@
+//This function checks if the user has entered a name - returns an elert if not
+//This function displays a message to the user when name and email are entered correctly
+//html checks if email is in the correct format
 function myInputs(email,userName,myDiv){	
 	var email = document.getElementById("email").value;
 	var userName = document.getElementById("userName").value;
--- script/scriptJQ.js
+++ script/scriptJQ.js
@@ -1,3 +1,4 @@
+//jquery function to allow user to press a button to see more information
 $(document).ready(function(){
   $("button").click(function(){
     $("#div1").fadeIn();
