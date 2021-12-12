8ea50db850e66b64da6e77ce227133c5d20c4b4b
laineysu <63746471+laineysu@users.noreply.github.com>
2021-12-12 17:21:35 +0000

    js validation to subscribe

--- script/script.js
+++ script/script.js
@@ -2,5 +2,10 @@ function myInputs(email,userName,myDiv){
 	var email = document.getElementById("email").value;
 	var userName = document.getElementById("userName").value;
 	document.getElementById("myForm").style.display="none";
-	alert(userName+" thank you for your details. We will be in touch via "+email+" shortly.");	
+	if (userName == "") {
+		alert("Name must be filled out");
+	}
+	else{
+		alert(userName+" thank you for your details. We will be in touch via "+email+" shortly.");	
+	}
 }
